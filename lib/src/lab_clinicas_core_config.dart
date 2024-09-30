b6f5601bd8442478f4a97a6b19ecff4726782ba0
victor <victor.alexandre@atlantasistemas.com>
2024-09-30 10:28:15 -0300

    Criacao do core config

--- lib/src/lab_clinicas_core_config.dart
+++ lib/src/lab_clinicas_core_config.dart
@@ -1,12 +1,48 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:fe_lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 
 class LabClinicasCoreConfig extends StatelessWidget {
-  const LabClinicasCoreConfig({super.key});
+  const LabClinicasCoreConfig({
+    super.key,
+    this.bindings,
+    this.pages,
+    this.pagesBuilder,
+    this.modules,
+    required this.title,
+  });
+
+  final ApplicationBindings? bindings;
+  final List<FlutterGetItPageRouter>? pages;
+  final List<FlutterGetItPageBuilder>? pagesBuilder;
+  final List<FlutterGetItModule>? modules;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Home Básica'),
+    return FlutterGetIt(
+      bindings: bindings,
+      pages: [
+        ...pages ?? [],
+        ...pagesBuilder ?? [],
+      ],
+      modules: modules,
+      builder: (context, routes, flutterGetItNavObserver) {
+        return AsyncStateBuilder(
+          loader: LabClinicasLoader(),
+          builder: (navigatorObserver) {
+            return MaterialApp(
+              navigatorObservers: [
+                navigatorObserver,
+                flutterGetItNavObserver,
+              ],
+              routes: routes,
+              title: title,
+            );
+          },
+        );
+      },
     );
   }
 }
