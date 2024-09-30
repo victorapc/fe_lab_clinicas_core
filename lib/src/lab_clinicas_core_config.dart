cec2bee2b088b52aff865f561a03f22368f8ef37
victor <victor.alexandre@atlantasistemas.com>
2024-09-30 09:14:38 -0300

    Atualização core

---
+++ lib/src/lab_clinicas_core_config.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LabClinicasCoreConfig extends StatelessWidget {
+  const LabClinicasCoreConfig({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text('Home Básica'),
+    );
+  }
+}
